6114ae8e24248169e5cdb818ef51046e7ff65b0b
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-02-01 22:45:20 +1000

    Update 'Node' output method

--- self_organizing_lists/node.py
+++ self_organizing_lists/node.py
@@ -14,7 +14,7 @@ class Node:
         next = self.next.data if self.next else None
         prev = self.prev.data if self.prev else None
         return (f'{self.__class__.__name__}('
-                f'{self.data!r}, {next!r}, {prev!r})')
+                f'{self.data!r}, next={next!r}, prev={prev!r})')
 
     def __str__(self):
         """
