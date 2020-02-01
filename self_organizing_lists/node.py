0459d4ecbc0bf0c661e008312a61d56090f945d0
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-02-01 22:26:43 +1000

    Implement 'Node' output methods

--- self_organizing_lists/node.py
+++ self_organizing_lists/node.py
@@ -6,3 +6,18 @@ class Node:
         self.data = data
         self.next = next
         self.prev = prev
+
+    def __repr__(self):
+        """
+        Return an unambiguous representation of an object.
+        """
+        next = self.next.data if self.next else None
+        prev = self.prev.data if self.prev else None
+        return (f'{self.__class__.__name__}('
+                f'{self.data!r}, {next!r}, {prev!r})')
+
+    def __str__(self):
+        """
+        Return value as a string.
+        """
+        return f'{self.data}'
