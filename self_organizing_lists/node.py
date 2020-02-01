0a957782fc66e28a321fc6a3c75abaf32b4bb3e9
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-02-01 21:37:55 +1000

    Add doc comment to 'Node' constructor

--- self_organizing_lists/node.py
+++ self_organizing_lists/node.py
@@ -1,5 +1,8 @@
 class Node:
     def __init__(self, data, next=None, prev=None):
+        """
+        Create a new list node.
+        """
         self.data = data
         self.next = next
         self.prev = prev
